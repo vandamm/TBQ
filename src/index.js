f59813599096cbe032027ad59e5534151b7ac658
Van Damm <EvilGreedyStalker@gmail.com>
2016-02-05 21:33:43 +0300

    Updated module exports to return actions instead of character

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-'use strict';
+'use strict'
 
-import createGame from './game';
-import character from './character';
+import createGame from './game'
+import {actions} from './character'
 
 module.exports = {
   createGame,
-  character
-};
+  actions
+}
